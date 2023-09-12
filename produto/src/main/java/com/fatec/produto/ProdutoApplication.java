d699b2b57195b43109a2615c14af9f4ece30cbbc
dti <dti@LAB204-27>
2023-09-12 09:00:12 -0300

    commit de teste

--- produto/src/main/java/com/fatec/produto/ProdutoApplication.java
+++ produto/src/main/java/com/fatec/produto/ProdutoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProdutoApplication {
+	
+	/** bomba **/
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutoApplication.class, args);
